11d96566adca7079315b83b5305a807aecf51fd7
Wietze <wietze@users.noreply.github.com>
2023-08-17 16:21:39 +0100

    Fixing incorrect TD tag

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1,5 +1,5 @@
 <?= sprintf(__('Total count of subscriptions: %d'), $settings['email_count']);
-print('<br /><br /><div style="height: 200px; overflow-y: scroll;"><table style="width: 100%;"><thead style="font-weight: bold;"><tr><th>Email address</th><th>Since</td></tr></thead>');
+print('<br /><br /><div style="height: 200px; overflow-y: scroll;"><table style="width: 100%;"><thead style="font-weight: bold;"><tr><th>Email address</th><th>Since</th></tr></thead>');
 foreach ($settings['subscribers'] as $subscriber) {
     printf('<tr><td>%s</td><td><time datetime="%s">%s</time></td></tr>', $subscriber['email'], mysql2date('c', $subscriber['date']), mysql2date(get_option('date_format'), $subscriber['date']));
 }
