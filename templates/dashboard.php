f0ace424048ee381103c1049c0548900ef0e93fd
Rafał Toborek <kontakt@toborek.info>
2023-08-21 00:08:35 +0200

    Added separated CSS for wp-admin dashboard panel

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1,7 +1,28 @@
-<?= sprintf(__('Total count of subscriptions: %d'), $settings['email_count']);
-print('<br /><br /><div style="height: 200px; overflow-y: scroll;"><table style="width: 100%;"><thead style="font-weight: bold;"><tr><th>Email address</th><th>Since</th></tr></thead>');
-foreach ($settings['subscribers'] as $subscriber) {
-    printf('<tr><td>%s</td><td><time datetime="%s">%s</time></td></tr>', $subscriber['email'], mysql2date('c', $subscriber['date']), mysql2date(get_option('date_format'), $subscriber['date']));
-}
-print('</table></div>');
-?>
+<h3><?= sprintf(__('Total count of subscriptions: %d'), $settings['email_count']) ?></h3>
+
+<?php if (count($settings['subscribers']) > 0): ?>
+    <div class="wp-lemme-know-admin-dashboard-list">
+        <table>
+            <thead>
+                <tr>
+                    <th><?= __('Email address') ?></th>
+                    <th><?= __('Since') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($settings['subscribers'] as $index => $subscriber): ?>
+                    <tr>
+                        <td><a href="mailto:<?= $subscriber['email'] ?>"><?= $subscriber['email'] ?></a></td>
+                        <td>
+                            <time datetime="<?= mysql2date('c', $subscriber['date']) ?>"><?= sprintf(
+                                '%s, %s',
+                                mysql2date(get_option('date_format'), $subscriber['date']),
+                                mysql2date(get_option('time_format'), $subscriber['date'])
+                            ) ?></time>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+<?php endif ?>
